695ee71c4091341b9757c4c7054006d272ebd84d
HaeengIn <129186569+HaeengIn@users.noreply.github.com>
2024-04-24 06:02:06 +0000

    delete var

--- Python/Account/function.py
+++ Python/Account/function.py
@@ -1,26 +1,23 @@
-def menu():
-    signup_id = "null"
-    signup_password = "null"
-    login_id = "null"
-    login_password = "null"
+menu = "null"
+id = "null"
+password = "null"
 
+def start():
     print("Login / SignUp")
-    choose = input("Choose Menu: ")
-    if choose == "Login":
+    menu = input("Choose Menu: ")
+
+def menu():
+    if menu == "Login":
         login()
-    if choose == "SignUp":
+    if menu == "SignUp":
         signup()
     else:
         print("Invaild Input")
 
 def signup():
-    signup_id = input("Enter New ID: ")
-    if signup_id == login_id:
-        print("This ID already exists.")
-    else:
-        login_id = signup_id
-        signup_password = input("Enter New Password: ")
-        login_password = signup_password
+    id = input("Enter New ID: ")
+    password = input("Enter New Password: ")
+    menu()
 
 def login():
     login_id = input("Enter ID: ")
