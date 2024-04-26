6637e0dce929560a0e950429c8c84a6caea24379
HaeengIn <129186569+HaeengIn@users.noreply.github.com>
2024-04-26 08:19:35 +0000

    add signup

--- Python/Account/main.py
+++ Python/Account/main.py
@@ -4,4 +4,18 @@ login_password = "null"
 signup_id = "null"
 signup_password = "null"
 
+def menu():
+    print("Login / SignUp")
+    menu = print("Choose Menu: ")
+    if menu == "SignUp":
+        signup_id = input("Enter New ID: ")
+        signup_password = input("Enter New Password")
+        print("SignUp Complete!")
+    if menu == "Login":
+        login_id = input("Enter ID: ")
+        login_password = input("Enter Password: ")
+        if login_id == signup_id:
+            print("Login Complete!")
+            
 print("---Make Your Account---")
+menu()
