13edc39da760bd8f35d617da32a9b41dff033ed7
HaeengIn <129186569+HaeengIn@users.noreply.github.com>
2024-05-14 02:43:05 +0000

    fix something

--- Python/Account/main.py
+++ Python/Account/main.py
@@ -1,12 +1,11 @@
-menu = "null"
-login_id = "null"
-login_password = "null"
-signup_id = "null"
-signup_password = "null"
+login_id = ""
+login_password = ""
+signup_id = ""
+signup_password = ""
 
 def menu():
     print("Login / SignUp")
-    menu = print("Choose Menu: ")
+    menu = input("Choose Menu: ")
     if menu == "SignUp":
         signup_id = input("Enter New ID: ")
         signup_password = input("Enter New Password")
@@ -16,6 +15,8 @@ def menu():
         login_password = input("Enter Password: ")
         if login_id == signup_id:
             print("Login Complete!")
-            
+            menu()
+        if login_id != signup_id:
+            print("Incorrect ID!")
 print("---Make Your Account---")
 menu()
