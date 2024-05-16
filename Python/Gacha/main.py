df66b0c6e55c532bcd1501ffd7b8c016d3f43ee9
HaeengIn <129186569+HaeengIn@users.noreply.github.com>
2024-05-16 06:47:19 +0000

    add korean

--- Python/Gacha/main.py
+++ Python/Gacha/main.py
@@ -40,7 +40,7 @@ def options():
         main()
     
 def main():
-    print("Start / Options / Exit")
+    print("Start / Options / Exit / 한국어")
     a = input()
     if a == "Start":
         run()
@@ -55,8 +55,66 @@ def main():
             main()
         else:
             print("Invalid Input")
+    if a == "한국어":
+        main_kor()
     else:
         print("Invaild Input")
-        
+
+def again_kor():
+    restart = input("다시 뽑을까요?(예/아니오) ")
+    if restart == "예":
+        run_kor()
+    if restart == "아니오":
+        main_kor()
+    else:
+        print("잘못된 입력값입니다")
+
+def run_kor():
+    while True:
+        global student
+        global num
+        global restart
+        if student == "":
+            student = int(input("마지막 번호를 입력해주세요: "))
+            num = ran.randint(1, student)
+            print(num)
+            again_kor()
+        if student != "":
+            num = ran.randint(1, student)
+            print(num)
+            again_kor()
+
+def options_kor():
+    print("수정 / 뒤로가기")
+    b = input("")
+    if b == "수정":
+        global student
+        student = int(input("수정할 마지막 번호: "))
+        print("수정되었습니다")
+        main_kor()
+    if b == "뒤로가기":
+        main_kor()
+
+def main_kor():
+    print("시작 / 옵션 / 나가기 / English")
+    a = input()
+    if a == "시작":
+        run_kor()
+    if a == "옵션":
+        options_kor()
+    if a == "나가기":
+        c = input("프로그램을 종료할까요?(예/아니오) ")
+        if c == "예":
+            print("종료되었습니다")
+            exit()
+        if c == "아니오":
+            main_kor()
+        else:
+            print("잘못된 입력값임니다")
+    if a == "English":
+        main()
+    else:
+        print("잘못된 입력값임니다")
+
 print("---Random Numbers---")
 main()
