c124f45e842d72498128db91f12872f40d13776a
HaeengIn <129186569+HaeengIn@users.noreply.github.com>
2024-05-20 06:42:41 +0000

    add appendix

--- Python/Gacha/main.py
+++ Python/Gacha/main.py
@@ -1,12 +1,16 @@
+# random 함수 임포트
 import random as ran
 
+# 변수 선언
 student = ""
 num = ""
 restart = ""
 
+# Invaild Input 출력
 def invalid():
     print("Invalid Input")
 
+# 다시 뽑기 실행
 def again():
     restart = input("Restart?(y/n) ")
     if restart == "y":
@@ -16,7 +20,8 @@ def again():
     else:
         invalid()
         again()
-        
+
+# 랜덤 뽑기 실행
 def run():
     while True:
         global student
@@ -25,13 +30,14 @@ def run():
         if student == "":
             student = int(input("Last Number? "))
             num = ran.randint(1, int(student))
-            print("Picked Number:",num)
+            print("Picked Number:", num)
             again()
         if student != "":
             num = ran.randint(1, student)
-            print("Picked Number:",num)
+            print("Picked Number:", num)
             again()
 
+# 옵션 실행
 def options():
     print("Fix / Current / Back")
     b = input("")
@@ -44,38 +50,8 @@ def options():
         if student == "":
             print("Last Number Is Not Set")
             options()
-        if student != "":
-            print("Current Last Number:",student)
-            options()
-    if b == "Back":
-        main()
-    else:
-        invalid()
-        options()
-    
-def main():
-    print("Start / Options / Exit / 한국어")
-    a = input()
-    if a == "Start":
-        run()
-    if a == "Options":
-        options()
-    if a == "Exit":
-        c = input("Exit Program?(y/n) ")
-        if c == "y":
-            print("Program Stopped")
-            exit()
-        if c == "n":
-            main()
-        else:
-            invalid()
-            main()
-    if a == "한국어":
-        main_kor()
-    else:
-        invalid()
-        main()
 
+# 다시 뽑기 실행
 def again_kor():
     restart = input("다시 뽑을까요?(예/아니오) ")
     if restart == "예":
@@ -85,6 +61,7 @@ def again_kor():
     else:
         print("잘못된 입력값입니다")
 
+# 랜덤 뽑기 실행
 def run_kor():
     while True:
         global student
@@ -100,6 +77,7 @@ def run_kor():
             print(num)
             again_kor()
 
+# 옵션 실행행
 def options_kor():
     print("수정 / 현황 / 뒤로가기")
     b = input("")
@@ -113,11 +91,12 @@ def options_kor():
             print("마지막 번호가 설정되어 있지 않습니다.")
             options_kor()
         if student != "":
-            print("현재 설정된 마지막 번호:",student)
+            print("현재 설정된 마지막 번호:", student)
             options_kor()
     if b == "뒤로가기":
         main_kor()
 
+# 메인 실행
 def main_kor():
     print("시작 / 옵션 / 나가기 / English")
     a = input()
@@ -139,5 +118,6 @@ def main_kor():
     else:
         print("잘못된 입력값임니다")
 
+# 메인 실행
 print("---Random Numbers---")
 main()
